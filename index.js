0d8c4967f2572f7deefab03fb3aa516eb271a00e
Yadavprash <py8986412056@gmail.com>
2023-09-06 14:01:39 +0530

    build 2.0

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ const crypto = require('crypto')
 require('dotenv').config()
 
 const userDatabase=[]
-const userListing={}
 const exercisesDatabase={}
-//Test Area
 
-
-
-
-
+function findUser(_id){
+  for(const user of userDatabase){
+    if(user._id === _id){
+      return user;
+    }
+  }
+  return null;
+}
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
-app.use(express.static('public'))
+app.use(express.static(__dirname +'/public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
@@ -31,8 +33,6 @@ app.post('/api/users',(req, res)=>{
     username:username
   }
   userDatabase.push(user)
-  userListing[id]=username;
-  console.log(userListing)
   res.json({username:username,_id:id});
 })
 
@@ -43,7 +43,6 @@ app.get('/api/users',(req, res)=>{
 
 //Getting User Exercises
 app.post('/api/users/:_id/exercises',(req, res)=>{
-  // console.log(req.body)
   const { _id } = req.params;
   const { description , duration } = req.body;
   let { date } = req.body
@@ -54,26 +53,34 @@ app.post('/api/users/:_id/exercises',(req, res)=>{
     const day = today.getDate().toString().padStart(2,'0'); // Get the current day of the month
     date=`${year}-${month}-${day}`;
   }
+  //converting date to required date String
+  const dateObject = new Date(date);
+
+  const options = {
+    weekday: 'short', // Short weekday name (e.g., "Wed")
+    year: 'numeric',  // Full year (e.g., "2023")
+    month: 'short',  // Short month name (e.g., "Sep")
+    day: '2-digit',  // Zero-padded day of the month (e.g., "06")
+  };
+  const formattedDateString = dateObject.toLocaleDateString(undefined, options);
   const exercise={
-    description: description,
+    date: formattedDateString,
     duration: parseInt(duration),
-    date: date
+    description: description
   };
   if(!exercisesDatabase[_id]){
     exercisesDatabase[_id]=[]
   }
   exercisesDatabase[_id].push(exercise);
-  const currentUser = userListing[_id]
-  console.log(exercisesDatabase)
-  res.json({_id:_id,username:currentUser,date:date,duration:duration,description:description})
+  const currentUser = findUser(_id);
+  res.json({...currentUser,...exercise})
 })
 
 //Displaying User logs
 app.get('/api/users/:_id/logs',(req, res)=>{
   const { _id } = req.params;
-  const username = userListing[_id];
+  const currentUser = findUser(_id);
   let logs = exercisesDatabase[_id];
-
   const fromDate = req.query.from;
   const toDate = req.query.to;
   const limit = parseInt(req.query.limit);
@@ -89,7 +96,7 @@ app.get('/api/users/:_id/logs',(req, res)=>{
   if (limit) {
     logs = logs.slice(0, limit);
   }
-  res.json({_id:_id,username:username,count:count,log:logs});
+  res.json({_id:_id,username:currentUser.username,count:count,log:logs});
 })
 
 const listener = app.listen(process.env.PORT || 3000, () => {
